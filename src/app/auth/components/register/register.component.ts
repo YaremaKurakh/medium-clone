12c3eca9ecb861bee96b102a22dfeedd441b4164
Yarema <jeremaker00@gmail.com>
2023-10-09 17:33:30 +0300

    Edit prettier, add auth module and register component

---
+++ src/app/auth/components/register/register.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+
+@Component({
+  selector: "mc-register",
+  templateUrl: "register.component.html",
+  styleUrls: ["register.component.scss"],
+})
+export class RegisterComponent implements OnInit {
+  form!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.form = this.fb.group({
+      userName: ["", Validators.required],
+      email: "",
+      password: "",
+    });
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
+  }
+}
