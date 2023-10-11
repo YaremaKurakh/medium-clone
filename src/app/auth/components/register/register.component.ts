89faaea8d595a90054e9e6209b917715202df3b3
Yarema <jeremaker00@gmail.com>
2023-10-11 16:03:47 +0300

    Added backend error messages display

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -3,8 +3,12 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { select, Store } from "@ngrx/store";
 import { registerAction } from "../../store/actions/register.action";
 import { Observable } from "rxjs";
-import { isSubmittingSelector } from "../../store/selectors";
+import {
+  isSubmittingSelector,
+  validationErrorsSelector,
+} from "../../store/selectors";
 import { RegisterRequestInterface } from "../../types/registerRequest.interface";
+import { BackendErrorsInterface } from "../../../shared/types/backendErrors.interface";
 
 @Component({
   selector: "mc-register",
@@ -14,6 +18,7 @@ import { RegisterRequestInterface } from "../../types/registerRequest.interface"
 export class RegisterComponent implements OnInit {
   form!: FormGroup;
   isSubmitting$!: Observable<boolean>;
+  backendErrors$!: Observable<BackendErrorsInterface | null>;
 
   constructor(
     private fb: FormBuilder,
@@ -27,6 +32,9 @@ export class RegisterComponent implements OnInit {
 
   initializeValues() {
     this.isSubmitting$ = this.store.source!.pipe(select(isSubmittingSelector));
+    this.backendErrors$ = this.store.source!.pipe(
+      select(validationErrorsSelector),
+    );
   }
 
   initializeForm() {
